e326e728eb66f535764b30fea12b5d756656e451
HuyPhong-21012889-PKA <131637318+HuyPhong-21012889-PKA@users.noreply.github.com>
2024-07-19 21:33:32 +0700

    game

---
+++ Assets/Scripts/Game/PathEvents.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace TowerDefence
+{
+    public class PathEvents : MonoBehaviour
+    {
+        public GameObject bombPrefab;
+
+        private void OnMouseDown()
+        {
+            if (EventSystem.current.IsPointerOverGameObject())
+                return;
+
+            if (Utils.Utilities.IsPointerOverUIObject())
+                return;
+
+            if (GameItemManager.activeItem)
+            {
+                RaycastHit hit;
+                Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+                if (Physics.Raycast(ray, out hit))
+                {
+                    print(GameItemManager.activeItem);
+                    PlayerDataManager.BombItemAmount -= 1;
+                    GameItemManager.activeItem = false;
+
+                    Instantiate(bombPrefab, hit.point, bombPrefab.transform.rotation);
+                }
+            }
+        }
+    }
+}
